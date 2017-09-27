f55a08d6a3cea0f6df95ee63be39fc7941738ed5
raideer <idmania1@gmail.com>
2017-09-27 13:29:47 +0300

    PEP8 compliant

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
+from flask import Flask, render_template, redirect, url_for
+from flask import flash, request, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
 
 from auth import OAuthSignIn, GoogleSignIn
@@ -15,31 +16,40 @@ lm.init_app(app)
 with app.app_context():
     db.create_all()
 
-# This method will be called whenever a guest tries to access user restricted page
+
+# This method will be called whenever a guest tries to access user restricted
+# page
 @lm.unauthorized_handler
 def unauthorized():
     flash('Please log in to access that page', 'alert-danger')
     return redirect(url_for('login'))
 
+
 @app.route("/")
 def index():
     categories = Category.query.all()
     latest_items = Item.query.order_by(db.desc('created_at')).limit(3)
-    return render_template('pages/index.html', categories = categories, latest_items = latest_items)
+    return render_template('pages/index.html',
+                           categories=categories, latest_items=latest_items)
+
 
 @app.route("/catalog.json")
 def json():
     categories = Category.query.all()
     return jsonify(Category=[i.serialize for i in categories])
 
+
 @app.route("/catalog/<slug>")
 def catalogItems(slug):
     categories = Category.query.all()
     catalog = Category.query.filter_by(slug=slugify(slug)).first()
-    return render_template('pages/catalog.html', catalog = catalog, categories = categories)
+    return render_template('pages/catalog.html', catalog=catalog,
+                           categories=categories)
 
-# Since this page should be accessed only by users, we can make use of the flask-login package
-# and add the @login_required decorator to handle this for us
+
+# Since this page should be accessed only by users, we can make use of the
+# flask-login package and add the @login_required decorator to handle this
+# functionality
 @app.route("/catalog/create", methods=['GET', 'POST'])
 @login_required
 def createCatalog():
@@ -53,12 +63,14 @@ def createCatalog():
             errors['name'] = "Category name is required!"
 
         if len(errors) == 0:
-            user = User.query.filter_by(id = current_user.get_id()).one()
-            category = Category(name = form_name, description = form_description, user = user, slug = slugify(form_name))
+            user = User.query.filter_by(id=current_user.get_id()).one()
+            category = Category(name=form_name, description=form_description,
+                                user=user, slug=slugify(form_name))
             db.session.add(category)
             db.session.commit()
             return redirect(url_for('catalogItems', slug=category.slug))
     return render_template('pages/create_catalog.html', errors=errors)
+
 
 @app.route("/catalog/<catalog_slug>/delete", methods=['GET', 'POST'])
 @login_required
@@ -71,7 +83,8 @@ def deleteCatalog(catalog_slug):
         return redirect(url_for('index'))
 
     if len(catalog.items) > 0:
-        flash('Cant delete %s because it is not empty!' % catalog.name, 'alert-danger')
+        flash('Cant delete %s because it is not empty!' % catalog.name,
+              'alert-danger')
         return redirect(url_for('catalogItems', slug=catalog_slug))
 
     if request.method == 'POST':
@@ -81,6 +94,7 @@ def deleteCatalog(catalog_slug):
         return redirect(url_for('index'))
 
     return render_template('pages/delete_catalog.html', catalog=catalog)
+
 
 @app.route("/item/create", defaults={'catalog': None}, methods=['GET', 'POST'])
 @app.route("/item/<catalog>/create", methods=['GET', 'POST'])
@@ -100,7 +114,7 @@ def createCatalogItem(catalog):
             errors['category'] = "Category is required!"
 
         try:
-            category = Category.query.filter_by(name = form_category).one()
+            category = Category.query.filter_by(name=form_category).one()
         except Exception as e:
             errors['category'] = "That is not a valid category!"
 
@@ -110,15 +124,21 @@ def createCatalogItem(catalog):
         if len(errors) > 0:
             if form_category:
                 catalog = form_category
-            return render_template('pages/create_item.html', categories=categories, catalog=catalog, errors=errors, name=form_name, description=form_description)
+            return render_template('pages/create_item.html',
+                                   categories=categories, catalog=catalog,
+                                   errors=errors, name=form_name,
+                                   description=form_description)
         else:
-            user = User.query.filter_by(id = current_user.get_id()).one()
-            item = Item(name = form_name, description = form_description, category = category, user = user)
+            user = User.query.filter_by(id=current_user.get_id()).one()
+            item = Item(name=form_name, description=form_description,
+                        category=category, user=user)
             db.session.add(item)
             db.session.commit()
             flash('Item successfuly created', 'alert-info')
             return redirect(url_for('catalogItems', slug=category.slug))
-    return render_template('pages/create_item.html', categories=categories, catalog=catalog, errors=errors)
+    return render_template('pages/create_item.html', categories=categories,
+                           catalog=catalog, errors=errors)
+
 
 @app.route("/item/<item_id>/delete", methods=['GET', 'POST'])
 @login_required
@@ -126,15 +146,17 @@ def deleteItem(item_id):
     item = Item.query.filter_by(id=item_id).first()
 
     if item.user != current_user:
-        flash('You don\'t have the permissions to delete %s' % item.name, 'alert-danger')
+        flash('You don\'t have the permissions to delete %s' % item.name,
+              'alert-danger')
         return redirect(url_for('index'))
 
     if request.method == 'POST':
         db.session.delete(item)
         db.session.commit()
         flash('Item successfuly deleted', 'alert-info')
-        return redirect(url_for('index'));
+        return redirect(url_for('index'))
     return render_template('pages/delete_item.html', item=item)
+
 
 @app.route("/item/<item_id>/edit", methods=['GET', 'POST'])
 @login_required
@@ -144,7 +166,8 @@ def editItem(item_id):
     item = Item.query.filter_by(id=item_id).first()
 
     if item.user != current_user:
-        flash('You don\'t have the permissions to edit %s' % item.name, 'alert-danger')
+        flash('You don\'t have the permissions to edit %s' % item.name,
+              'alert-danger')
         return redirect(url_for('index'))
 
     if request.method == 'POST':
@@ -158,7 +181,7 @@ def editItem(item_id):
             errors['category'] = "Category is required!"
 
         try:
-            category = Category.query.filter_by(name = form_category).one()
+            category = Category.query.filter_by(name=form_category).one()
         except Exception as e:
             errors['category'] = "That is not a valid category!"
 
@@ -168,21 +191,26 @@ def editItem(item_id):
         if len(errors) > 0:
             if form_category:
                 catalog = form_category
-            return render_template('pages/edit_item.html', categories=categories, errors=errors, item=item)
+            return render_template('pages/edit_item.html',
+                                   categories=categories, errors=errors,
+                                   item=item)
         else:
-            item.name = form_name;
-            item.category = category;
-            item.description = form_description;
+            item.name = form_name
+            item.category = category
+            item.description = form_description
 
             db.session.add(item)
             db.session.commit()
             return redirect(url_for('catalogItems', slug=category.slug))
 
-    return render_template('pages/edit_item.html', categories=categories, errors=errors, item=item)
+    return render_template('pages/edit_item.html', categories=categories,
+                           errors=errors, item=item)
+
 
 @app.route("/catalog/<catalog>/<item>")
 def catalogItem(catalog, item):
     return "'%s' description" % item
+
 
 # Handles oAuth URL generation and redirecting
 # Check out auth.py for more details
@@ -192,7 +220,8 @@ def oauth_authorize(provider):
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize();
+    return oauth.authorize()
+
 
 # Handles callbacks from oAuth providers
 @app.route('/callback/<provider>')
@@ -202,8 +231,8 @@ def oauth_callback(provider):
         return redirect(url_for('index'))
 
     oauth = OAuthSignIn.get_provider(provider)
-    # oauth.callback() processes received request arguments, creates a new 'rauth'
-    # oAuth session and returns the basic user credentials (id, username, email)
+    # oauth.callback() processes received request arguments, creates a new
+    # oAuth session and returns the basic user credentials(id, username, email)
     social_id, username, email = oauth.callback()
     if social_id is None:
         flash('Authentication failed.')
@@ -216,18 +245,21 @@ def oauth_callback(provider):
     login_user(user, True)
     return redirect(url_for('index'))
 
+
 @app.route('/login')
 def login():
     if current_user is not None and current_user.is_authenticated:
         return redirect(url_for('index'))
     return render_template('pages/login.html')
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
     app.secret_key = 'asecretkey'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port=8000)
